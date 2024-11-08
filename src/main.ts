58204b86337ed598a8bcc77de858f1318c97c955
Nikolay <kolyak770@gmail.com>
2024-11-08 19:50:45 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +11,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const yaml = await readFile('./doc/api.yaml', 'utf-8');
+
+  SwaggerModule.setup('doc', app, parse(yaml));
 
   await app.listen(process.env.PORT ?? 4000);
 }
